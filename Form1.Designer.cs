79221d33fa56e52b5035a46efe48177fecd3c642
CDStefan <caraveleastefan@gmail.com>
2024-05-04 15:07:03 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Cs_addin_pen_MST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenWorkbook1 = new System.Windows.Forms.Button();
+            this.btnOpenWorkbook2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenWorkbook1
+            // 
+            this.btnOpenWorkbook1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenWorkbook1.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenWorkbook1.Name = "btnOpenWorkbook1";
+            this.btnOpenWorkbook1.Size = new System.Drawing.Size(556, 70);
+            this.btnOpenWorkbook1.TabIndex = 0;
+            this.btnOpenWorkbook1.Text = "Registre_evidenta_asesizarilor_confirmarea_autorizarea_interceptarilor";
+            this.btnOpenWorkbook1.UseVisualStyleBackColor = true;
+            this.btnOpenWorkbook1.Click += new System.EventHandler(this.btnOpenWorkbook1_Click);
+            // 
+            // btnOpenWorkbook2
+            // 
+            this.btnOpenWorkbook2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenWorkbook2.Location = new System.Drawing.Point(12, 125);
+            this.btnOpenWorkbook2.Name = "btnOpenWorkbook2";
+            this.btnOpenWorkbook2.Size = new System.Drawing.Size(556, 70);
+            this.btnOpenWorkbook2.TabIndex = 1;
+            this.btnOpenWorkbook2.Text = "Registre_evidenta_autorizatiilor_perchezitie_domiciliara_UP";
+            this.btnOpenWorkbook2.UseVisualStyleBackColor = true;
+            this.btnOpenWorkbook2.Click += new System.EventHandler(this.btnOpenWorkbook2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 251);
+            this.Controls.Add(this.btnOpenWorkbook2);
+            this.Controls.Add(this.btnOpenWorkbook1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenWorkbook1;
+        private System.Windows.Forms.Button btnOpenWorkbook2;
+    }
+}
---
+++ ThisAddIn.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Cs_addin_pen_MST
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            //this.Application.WorkbookOpen += new Excel.AppEvents_WorkbookOpenEventHandler(WorkBookOpen);
+            this.Application.WorkbookBeforeSave += new Excel.AppEvents_WorkbookBeforeSaveEventHandler(Application_WorkbookBeforeSave);
+            this.Application.WorkbookAfterSave += new Excel.AppEvents_WorkbookAfterSaveEventHandler(Application_WorkbookAfterSave);
+
+
+            Form1 openForm = new Form1();
+            openForm.TopMost = true;
+            openForm.Show();
+
+
+        }
+
+        // Prevent the user from savind to a different file without password
+        void Application_WorkbookBeforeSave(Excel.Workbook Wb, bool SaveAsUI, ref bool Cancel)
+        {
+            Cancel = true; // Prevent the normal save process
+            Wb.Application.EnableEvents = false;
+            Wb.SaveAs(Wb.FullName, Password: "Timis-1864"); // Save with the original password
+            Wb.Application.EnableEvents = true;
+        }
+
+        // Try to make a backup after saving the changes
+        void Application_WorkbookAfterSave(Excel.Workbook Wb, bool Success)
+        {
+            string backupPath = "";
+            try
+            {
+                Excel.Worksheet settingsSheet = Wb.Sheets["Settings"];
+                Excel.Range backupPathCell = settingsSheet.Range["B1"];
+                backupPath = backupPathCell.Value;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Could not fined value of path: {ex.Message}");
+                
+            }
+
+            if (!Directory.Exists(backupPath))
+            {
+                MessageBox.Show("Folderul de backup nu a fost gasit. Contactati compartimentul informatica");
+            }
+            else
+            {
+                string backupFileName = $"{DateTime.Now.ToString("dd-MM")}-{Wb.Name}-backup";
+                string backupFilePath = Path.Combine(backupPath, backupFileName);
+                Wb.SaveCopyAs(backupFilePath);
+            }
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            this.Application.WorkbookBeforeSave -= new Excel.AppEvents_WorkbookBeforeSaveEventHandler(Application_WorkbookBeforeSave);
+            this.Application.WorkbookAfterSave -= new Excel.AppEvents_WorkbookAfterSaveEventHandler(Application_WorkbookAfterSave);
+            
+
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
